1e8f17683adc08b937f25c8211a496d3d320fb40
ampoloquiomj <ampoloquioboisermaryjoy@gmail.com>
2025-09-20 10:23:26 +0800

    Added files for MidAct1

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import CounterApp from './CounterApp';
-import ColorChangerApp from './ColorChangerApp';
+import React from "react";
+import ChatScreen from "./ChatScreen";
+import CommentSection from "./CommentSection";
 
-const App = () => {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <CounterApp />
-            <ColorChangerApp />
-        </SafeAreaView>
-    );
-};
-
+export default function App() {
+  return (
+    <>
+      <ChatScreen />
+      <CommentSection />
+    </>
+  );
+}
 export default App;
